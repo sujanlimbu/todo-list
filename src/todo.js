43f005fd906df064bef52b9291f0cbd65f212a88
Sujan Khapung <sujanlimbu8@gmail.com>
2024-06-25 15:10:44 +1000

    todo and project classes added

--- src/todo.js
+++ src/todo.js
@@ -1,3 +1,17 @@
-export default function getTodo() {
-    console.log("todo is ready");
+class Todo {
+    constructor(title, description, dueDate, priority, notes = '', checklist = []) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+        this.checklist = checklist;
+        this.complete = false;
+    }
+
+    toggleComplete() {
+        this.complete = !this.complete;
+    }
 }
+
+export default Todo;
